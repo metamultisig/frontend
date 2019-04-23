a01667570d5bc93c0939dce364a1d1d981397ae2
Nick Johnson <arachnid@notdot.net>
2019-04-23 13:19:06 +1200

    Updates

---
+++ precache-manifest.b88c10e5794ae868bfc78f2cd2c98f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd924a9862891b98c97",
+    "url": "/frontend/static/js/runtime~main.f332d61a.js"
+  },
+  {
+    "revision": "b20f2a81468d3dd788c2",
+    "url": "/frontend/static/js/main.d392803f.chunk.js"
+  },
+  {
+    "revision": "6f19c741df0f471e0752",
+    "url": "/frontend/static/js/2.dfcc5770.chunk.js"
+  },
+  {
+    "revision": "b20f2a81468d3dd788c2",
+    "url": "/frontend/static/css/main.dd472a57.chunk.css"
+  },
+  {
+    "revision": "1ab381a60beacab996e90a95d8fa0957",
+    "url": "/frontend/index.html"
+  }
+];
